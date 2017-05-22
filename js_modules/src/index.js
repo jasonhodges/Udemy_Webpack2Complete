1db96e8c6366a41e27ccfdefbf9ab0a0f7d0f375
Jason Hodges <jason.hodges@clickfox.com>
2017-05-22 06:03:43 -0600

    initial commit

---
+++ js_modules/src/index.js
@@ -0,0 +1,4 @@
+/* Calls fucntions from sum.js,
+ * then prints the result.
+ * Depends on sum.js to be loaded.
+ */
---
+++ js_modules/src/sum.js
@@ -0,0 +1,5 @@
+/* utility functions for working with 
+ * mathmatical calculations.
+ */
+
+const sum = (a, b) => a + b;
